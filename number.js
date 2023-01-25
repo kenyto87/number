1ad6d36fed1ba035eb9e3f59e0e75731cd4a133c
Francisco David <35648594+kenyto87@users.noreply.github.com>
2023-01-24 22:34:30 -0300

    Add files via upload

---
+++ number.js
@@ -0,0 +1,6 @@
+let altCent = 172;
+let altMetros = 17.2;
+let peso = 80.5;
+let altRedon = altMetros.toFixed(2);
+let pesoRedon = peso.toFixed(0);
+let ifMaximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
